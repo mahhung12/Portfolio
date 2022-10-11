630217262af831755b33d0e030b4a82b21fdc63b
manhhung <mahhungg.12@gmail.com>
2022-10-11 09:16:13 +0700

    update custom modal

--- components/Pages/Home/components/Work/All/index.tsx
+++ components/Pages/Home/components/Work/All/index.tsx
@@ -2,28 +2,30 @@ import React, { useState } from "react";
 import { Button } from "antd";
 
 import Modal from "@components//Modal";
+import ModalContentCustom from "@components//Modal/ModalContentCustom";
 
 const All = () => {
-    const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(false);
 
-    const handleSetVisible = () => setVisible(true);
+  const handleSetVisible = () => setVisible(true);
 
-    const handleCloseModal = () => setVisible(false);
+  const handleCloseModal = () => setVisible(false);
 
-    return (
-        <div className="all-tab">
-            <Button onClick={handleSetVisible}>Open Modal</Button>
-            <Modal
-                visible={visible}
-                width={500}
-                onClose={handleCloseModal}
-                maskClosable={true}
-                showCloseIcon={true}
-            >
-                Content Tab All
-            </Modal>
-        </div>
-    );
+  return (
+    <div className="all-tab">
+      <Button onClick={handleSetVisible}>Open Modal</Button>
+      <Modal
+        visible={visible}
+        width={500}
+        onClose={handleCloseModal}
+        showCloseIcon
+        maskClosable
+        centered
+      >
+        <ModalContentCustom />
+      </Modal>
+    </div>
+  );
 };
 
 export default All;
