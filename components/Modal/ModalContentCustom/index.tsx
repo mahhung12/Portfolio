f6e8f7ebdb811225e2502cd1ae1fba6de02df407
manhhung <mahhungg.12@gmail.com>
2022-10-12 22:22:46 +0700

    update project modal

--- components/Modal/ModalContentCustom/index.tsx
+++ components/Modal/ModalContentCustom/index.tsx
@@ -7,75 +7,56 @@ import ItemWithLabel from "@components//ItemWithLabel";
 import { PROJECT_PROPERTIES } from "constants/author";
 
 type ModalContent = {
-    breadcrumbs?: unknown | any;
-    title?: string;
-    status?: string;
-    assignee?: string;
-    dueDate?: string;
-    tech?: string | any;
-    content?: ReactNode | any;
+  breadcrumbs?: unknown | any;
+  title?: string;
+  status?: string;
+  assignee?: string;
+  position?: string;
+  dueDate?: string;
+  tech?: string | any;
+  content?: ReactNode | any;
 };
 
 const { Item } = Breadcrumb;
-const { STATUS, ASSIGNEE, DUEDATE, TECHNIQUE } = PROJECT_PROPERTIES;
+const { STATUS, ASSIGNEE, POSITION, DUEDATE, TECHNIQUE } = PROJECT_PROPERTIES;
 
 const ModalContentCustom = ({
-    breadcrumbs,
-    title,
-    status,
-    assignee,
-    dueDate,
-    tech,
-    content,
-    ...props
+  breadcrumbs,
+  title,
+  status,
+  assignee,
+  position,
+  dueDate,
+  tech,
+  content,
+  ...props
 }: ModalContent) => {
-    return (
-        <Row className="modal-content-custom">
-            <Breadcrumb className="project-breadcrumb">
-                {breadcrumbs?.map((breadcrumb: string, idx: number) => (
-                    <Item key={idx}>{breadcrumb}</Item>
-                ))}
-            </Breadcrumb>
+  return (
+    <Row className="modal-content-custom">
+      <Breadcrumb className="project-breadcrumb">
+        {breadcrumbs?.map((breadcrumb: string, idx: number) => (
+          <Item key={idx}>{breadcrumb}</Item>
+        ))}
+      </Breadcrumb>
 
-            <EllipsisText text={title} className="project-title" />
+      <EllipsisText text={title} className="project-title" />
 
-            <Row className="project-properties">
-                <ItemWithLabel
-                    className="status"
-                    label={STATUS}
-                    children={status}
-                />
-
-                <ItemWithLabel
-                    className="technique"
-                    label={TECHNIQUE}
-                    children={tech}
-                />
-
-                <ItemWithLabel
-                    className="due_date"
-                    label={DUEDATE}
-                    children={dueDate}
-                />
-
-                <ItemWithLabel
-                    className="assignee"
-                    label={ASSIGNEE}
-                    children={assignee}
-                />
-            </Row>
-
-            <Row className="project-description">
-                <EllipsisText className="desc-title" text="Description" />
-
-                <span className="desc">{content}</span>
-            </Row>
-
-            <Row className="project-preview">
-                <EllipsisText className="preview-title" text="Preview" />
-            </Row>
-        </Row>
-    );
+      <Row className="project-properties">
+        <ItemWithLabel className="status" label={STATUS}>
+          {status}
+        </ItemWithLabel>
+        <ItemWithLabel className="position" label={POSITION}>
+          {position}
+        </ItemWithLabel>
+        <ItemWithLabel className="due_date" label={DUEDATE}>
+          {dueDate}
+        </ItemWithLabel>
+        <ItemWithLabel className="technique" label={TECHNIQUE}>
+          {tech}
+        </ItemWithLabel>
+      </Row>
+    </Row>
+  );
 };
 
 export default ModalContentCustom;
---
+++ constants/author_projects.ts
@@ -0,0 +1,15 @@
+import { USER_TABS } from "./author";
+import { PROJECT_STATUS } from "./common";
+
+const { ALL, BLOCKCHAIN, MAIN, OTHERS } = USER_TABS;
+const { PENDING, INPROGRESS, RELEASE, DONE } = PROJECT_STATUS;
+// const {};
+
+export const AUTHOR_PROJECTS = [
+  {
+    TYPE: BLOCKCHAIN,
+    TITLE: "C2C Whitelabel Marketplace",
+    STATUS: INPROGRESS,
+    // POSITION:
+  },
+];
