294682b063dafbb6462020700ab24e66cb378d6e
mhung286 <mahhungg.12@gmail.com>
2022-10-12 22:20:02 +0700

    update for coding

--- constants/author.ts
+++ constants/author.ts
@@ -5,54 +5,54 @@ import Linkedin from "public/svg/LinkedinNegative.svg";
 import Telegram from "public/svg/TelegramNegative.svg";
 
 export const AUTHOR_SOCIAL_MEDIA = [
-  {
-    name: "github",
-    icon: Github,
-  },
-  {
-    name: "instagram",
-    icon: Instagram,
-  },
-  {
-    name: "facebook",
-    icon: Facebook,
-  },
-  {
-    name: "telegram",
-    icon: Telegram,
-  },
-  {
-    name: "linkedin",
-    icon: Linkedin,
-  },
+    {
+        name: "github",
+        icon: Github,
+    },
+    {
+        name: "instagram",
+        icon: Instagram,
+    },
+    {
+        name: "facebook",
+        icon: Facebook,
+    },
+    {
+        name: "telegram",
+        icon: Telegram,
+    },
+    {
+        name: "linkedin",
+        icon: Linkedin,
+    },
 ];
 
 export const AUTHOR_SKILLS = [
-  "React JS",
-  "Next JS",
-  "Typescript",
-  "Node JS",
-  "HTML5 / CSS3 / Javascript",
-  "Figma, Photoshop...",
-  "Library (React JS: redux, zustand, antd...",
+    "React JS",
+    "Next JS",
+    "Typescript",
+    "Node JS",
+    "HTML5 / CSS3 / Javascript",
+    "Figma, Photoshop...",
+    "Library (React JS: redux, zustand, antd...",
 ];
 
 export const USER_TABS = {
-  ALL: "all",
-  BLOCKCHAIN: "blockchain",
-  MAIN: "main",
-  OTHERS: "others",
+    ALL: "all",
+    BLOCKCHAIN: "blockchain",
+    MAIN: "main",
+    OTHERS: "others",
 };
 
 export const AUTHOR_HEADER_TITLE = {
-  ABOUT: "About",
-  SKILLS: "Skills",
-  WORK: "Work",
+    ABOUT: "About",
+    SKILLS: "Skills",
+    WORK: "Work",
 };
 
 export const PROJECT_PROPERTIES = {
-  STATUS: "Status",
-  ASSIGNEE: "Assignee",
-  DUEDATE: "Due date",
-  TECHNIQUE: "Technique",
+    STATUS: "Status",
+    ASSIGNEE: "Assignee",
+    DUEDATE: "Date",
+    TECHNIQUE: "Technique",
 };
