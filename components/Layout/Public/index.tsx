a2a1c2d217347b9487280de7928b38c05ddf38ae
manhhung <mahhungg.12@gmail.com>
2022-10-17 17:28:25 +0700

    update form section

--- components/Layout/Public/index.tsx
+++ components/Layout/Public/index.tsx
@@ -5,7 +5,10 @@ import Image from "next/image";
 import AppSeo from "@components//AppSeo";
 import AppHeader from "@components//AppHeader";
 import AppFooter from "@components//AppFooter";
+import AppAudioBar from "@components//AppAudioBar";
 import LoadingIcon from "@components//LoadingIcon";
+
+import ChevronUpIcon from "public/svg/chevron_up_icon.svg";
 
 const Layout: FC<{
   children: any;
@@ -21,19 +24,21 @@ const Layout: FC<{
     <Spin indicator={<LoadingIcon />} spinning={false}>
       <AppSeo title={title} socialImageUrl={socialImageUrl} faviconImageUrl={faviconImageUrl} metaDescription={metaDescription} />
 
-      <div className={className}>
-        {!notShowHeader && <AppHeader />}
-        <div className="app-content">
-          <BackTop>
-            <div className="scroll-to-top">
-              <Image src="/" alt="" layout="fill" />
-            </div>
-          </BackTop>
-          {children}
-        </div>
+      <AppAudioBar>
+        <div className={className}>
+          {!notShowHeader && <AppHeader />}
+          <div className="app-content">
+            <BackTop>
+              <div className="scroll-to-top">
+                <Image src={ChevronUpIcon} alt="" layout="fill" />
+              </div>
+            </BackTop>
+            {children}
+          </div>
 
-        {!notShowFooter && <AppFooter />}
-      </div>
+          {!notShowFooter && <AppFooter />}
+        </div>
+      </AppAudioBar>
     </Spin>
   );
 };
